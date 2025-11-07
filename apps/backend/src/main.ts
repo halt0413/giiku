994b26069c434669d9ce0e4c5a9cb515b79c13de
hsmtDP-T <ittuyo.hashi244@gmail.com>
2025-11-08 03:53:54 +0900

    expressモジュールをインポートし、JSONリクエストの処理を追加しました

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(express.json());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
