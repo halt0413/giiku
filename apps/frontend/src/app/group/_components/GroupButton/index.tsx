f59d45611fa0157663917bd8eadb16271ac426b0
hal <halt0413@gmail.com>
2025-11-08 15:27:33 +0900

    グループボタンコンポーネントにクリックハンドラーを追加し、グループ作成と参加のアクションを実装しました。

--- apps/frontend/src/app/group/_components/GroupButton/index.tsx
+++ apps/frontend/src/app/group/_components/GroupButton/index.tsx
@@ -3,18 +3,29 @@
 import Image from 'next/image'
 import ActionButton from '@/components/ActionButton'
 import styles from './index.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function GroupButton() {
+  const router = useRouter()
+
+  const handleCreateGroup = () => {
+    router.push('/group/groupcreate')
+  }
+
+  const handleParticipateGroup = () => {
+    router.push('/group/groupparticipation')
+  }
+
   return (
     <div className={styles.container}>
-      <ActionButton>
+      <ActionButton onClick={handleCreateGroup}>
         <div className={styles.inner}>
           <span className={styles.text}>グループを作成する</span>
           <Image src="/group.svg" alt="グループ作成アイコン" width={32} height={32} />
         </div>
       </ActionButton>
 
-      <ActionButton>
+      <ActionButton onClick={handleParticipateGroup}>
         <div className={styles.inner}>
           <span className={styles.text}>グループに参加する</span>
           <Image src="/done.svg" alt="グループ参加アイコン" width={32} height={32} />
