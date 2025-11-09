32bf6cb7a1af0c8ace80d19cca2eb3ccc3e67f96
hal <halt0413@gmail.com>
2025-11-09 10:39:04 +0900

    feat: マップコンポーネントの追加とスタイルの定義

---
+++ apps/frontend/src/app/home/_components/mapview/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from 'react';
+import Map, { type ViewState } from 'react-map-gl';
+import styles from './index.module.css';
+
+const INITIAL_VIEW_STATE = {
+  longitude: 139.767125,
+  latitude: 35.681236,
+  zoom: 14,
+};
+
+export const MapComponent = () => {
+  const [viewState, setViewState] = useState<ViewState>(INITIAL_VIEW_STATE);
+
+  const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+  if (!mapboxToken) {
+    console.error("Mapbox Access Token が .env.local で設定されていません。");
+    return <div className={styles.error}>Mapbox トークンがありません。</div>;
+  }
+
+  return (
+    <div className={styles.mapContainer}>
+      <Map
+        {...viewState}
+        mapboxAccessToken={mapboxToken}
+        onMove={(evt) => setViewState(evt.viewState)}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        style={{ width: '100%', height: '100%' }}
+      />
+    </div>
+  );
+};
---
+++ apps/frontend/src/app/home/map/page.tsx
@@ -0,0 +1,9 @@
+import { MapComponent } from "../_components/mapview"
+
+export default function mappage() {
+  return (
+    <div>
+      <MapComponent />
+    </div>
+  )
+}
